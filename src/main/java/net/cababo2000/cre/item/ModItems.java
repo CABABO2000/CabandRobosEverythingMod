50be9ccf3634512797fec044b985c576b347f382
CABABO2000 <babodino08@gmail.com>
2025-05-22 14:32:15 -0700

    Added Starite; Currently has no function, plan to make it similar to Netherite. Changed MODID from cab&roboseverythingmod to cre.

---
+++ src/main/java/net/cababo2000/cre/item/ModItems.java
@@ -0,0 +1,20 @@
+package net.cababo2000.cre.item;
+
+import net.cababo2000.cre.c_r_e;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, c_r_e.MODID);
+
+    public static final RegistryObject<Item> STARITE = ITEMS.register("starite",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventbus) {
+        ITEMS.register(eventbus);
+    }
+}
