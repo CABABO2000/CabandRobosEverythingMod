f4ff4c0345551194fe80b625c74af6ba6d2d3b11
CABABO2000 <babodino08@gmail.com>
2025-05-25 01:01:35 -0700

    Added custom blocks tab.

--- src/main/java/net/cababo2000/cre/item/ModCreativeTabs.java
+++ src/main/java/net/cababo2000/cre/item/ModCreativeTabs.java
@@ -1,8 +1,10 @@
 package net.cababo2000.cre.item;
 
+import net.cababo2000.cre.block.ModBlocks;
 import net.cababo2000.cre.c_r_e;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -19,6 +21,16 @@ public class ModCreativeTabs {
                     .displayItems((pParameters, pOutput) -> {
                         pOutput.accept(ModItems.STARITE.get());
                         pOutput.accept(ModItems.ENDCOR.get());
+
+
+                    })
+                    .build());
+
+    public static final RegistryObject<CreativeModeTab> CRE_BLOCKS = CREATIVE_MODE_TABS.register("cre_block",
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModBlocks.STARITE_ORE.get()))
+                    .title(Component.translatable("creativetab.cre_blocks"))
+                    .displayItems((pParameters, pOutput) -> {
+                        pOutput.accept(ModBlocks.STARITE_ORE.get());
 
 
                     })
